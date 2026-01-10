20cae74be93f1c4af68a8b2514ff70b92811e0af
Ayman29996266 <ayman.29996266.ran@gmail.com>
2026-01-10 11:57:57 +0530

    A full rework started

--- example_usage.py
+++ example_usage.py
@@ -5,6 +5,6 @@ from cuims_scraper import scrape
 if __name__ == '__main__':
 
     
-    scrape('path',UID='23BIS70139', PASSWORD='01.Jan.2005',
-           download_folder='/home/ayman/Downloads/')
+    scrape(UID='23BIS70139', PASSWORD='01Jan2005@',
+           download_folder='/home/ayman/code/CUIMS-SCRAPER')
     
